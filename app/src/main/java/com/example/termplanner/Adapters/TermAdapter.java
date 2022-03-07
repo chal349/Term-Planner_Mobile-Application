91700a8aafedfd69db7d005f4a69ceed65063b8a
coreyhall <c7ha77@gmail.com>
2022-03-07 14:29:21 -0500

    added feature to search Terms

--- app/src/main/java/com/example/termplanner/Adapters/TermAdapter.java
+++ app/src/main/java/com/example/termplanner/Adapters/TermAdapter.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,9 +17,10 @@ import com.example.termplanner.Entities.Term;
 import com.example.termplanner.R;
 import com.example.termplanner.UI.TermDetailsActivity;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class TermAdapter extends RecyclerView.Adapter<TermAdapter.ViewHolder> {
+public class TermAdapter extends RecyclerView.Adapter<TermAdapter.ViewHolder> implements Filterable {
 
 
     class ViewHolder extends RecyclerView.ViewHolder {
@@ -50,10 +53,13 @@ public class TermAdapter extends RecyclerView.Adapter<TermAdapter.ViewHolder> {
     private final LayoutInflater inflater;
     private final Context context;
     private List<Term> termsList;
+    private List<Term> termsListFull;
 
-    public TermAdapter(Context context) {
+    public TermAdapter(Context context, List<Term> termsList) {
         inflater = LayoutInflater.from(context);
         this.context = context;
+        this.termsList = termsList;
+        termsListFull = new ArrayList<>(termsList);
     }
 
     @NonNull
@@ -85,4 +91,38 @@ public class TermAdapter extends RecyclerView.Adapter<TermAdapter.ViewHolder> {
     public int getItemCount() {
         return termsList.size();
     }
+
+    @Override
+    public Filter getFilter() {
+        return termFilter;
+    }
+
+    private Filter termFilter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            List<Term> filteredList = new ArrayList<>();
+
+            if(constraint == null || constraint.length() == 0) {
+                filteredList.addAll(termsListFull);
+            } else {
+                String filterPattern = constraint.toString().toLowerCase().trim();
+
+                for(Term term : termsListFull){
+                    if(term.getTitle().toLowerCase().contains(filterPattern)){
+                        filteredList.add(term);
+                    }
+                }
+            } FilterResults results = new FilterResults();
+            results.values = filteredList;
+
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+            termsList.clear();
+            termsList.addAll((List) filterResults.values);
+            notifyDataSetChanged();
+        }
+    };
 }
--- app/src/main/java/com/example/termplanner/UI/TermActivity.java
+++ app/src/main/java/com/example/termplanner/UI/TermActivity.java
@@ -1,11 +1,15 @@
 package com.example.termplanner.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.example.termplanner.Adapters.TermAdapter;
@@ -14,10 +18,12 @@ import com.example.termplanner.R;
 import com.example.termplanner.Repository.Repository;
 
 import java.util.List;
+import java.util.Locale;
 
 public class TermActivity extends AppCompatActivity {
 
     private Repository repository;
+    private TermAdapter termAdapter;
 
 
     @Override
@@ -32,7 +38,7 @@ public class TermActivity extends AppCompatActivity {
 
         RecyclerView recyclerView = findViewById(R.id.terms_recycler);
 
-        final TermAdapter termAdapter = new TermAdapter(this);
+        termAdapter = new TermAdapter(this, allTerms);
         recyclerView.setAdapter(termAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         termAdapter.setTerms(allTerms);
@@ -40,5 +46,41 @@ public class TermActivity extends AppCompatActivity {
 
     public void addTerm(View view) {
         startActivity(new Intent(TermActivity.this, TermAddActivity.class));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_menu, menu);
+
+        MenuItem.OnActionExpandListener onActionExpandListener = new MenuItem.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem menuItem) {
+                return true;
+            }
+
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem menuItem) {
+                return true;
+            }
+        };
+
+        menu.findItem(R.id.search).setOnActionExpandListener(onActionExpandListener);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String searchText) {
+                termAdapter.getFilter().filter(searchText);
+                return false;
+            }
+        });
+
+
+        return true;
     }
 }
